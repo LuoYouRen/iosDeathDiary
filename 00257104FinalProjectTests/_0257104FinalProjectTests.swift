8cd37c93e4c12f48f71ff28abd2dd874b442d45f
SZLforGithub <user_08@mac-08deimac.local>
2017-01-07 16:49:53 +0800

    Initial Commit

---
+++ 00257104FinalProjectTests/_0257104FinalProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  _0257104FinalProjectTests.swift
+//  00257104FinalProjectTests
+//
+//  Created by user_08 on 2017/1/7.
+//  Copyright © 2017年 00257104. All rights reserved.
+//
+
+import XCTest
+@testable import _0257104FinalProject
+
+class _0257104FinalProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
